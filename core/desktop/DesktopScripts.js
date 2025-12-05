e651c4983547fb5ce7fd921ba1bb56e0bd1eed51
VitalyK <vitaly.kontyshev@gmail.com>
2025-12-05 12:29:42 +0300

    Delete core directory

--- core/desktop/DesktopScripts.js
+++
@@ -1,9 +0,0 @@
-function updateClock() {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const separator = now.getSeconds() % 2 === 0 ? ':' : ' ';
-  document.getElementById('clock').textContent = `${hours}${separator}${minutes}`;
-}
-setInterval(updateClock, 1000);
-updateClock();
